818ca634d5f056cac22ad5e407bc03d016a4202c
Mike Munroe <mike@mikemunroe.com>
2014-04-09 22:11:30 -0400

    model returns sample data for now

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,1 +1,10 @@
-export default Ember.Route.extend({});
+export default Ember.Route.extend({
+  model: function() {
+    return [{
+      id: 1,
+      title: 'Flash Boys',
+      author: 'Michael Lewis',
+      review: 'Great coverage of HFT on Wall Street. Michael Lewis always writes hits.'
+    }];
+  }
+});
