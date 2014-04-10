40f1588ba91c8b0dc6cc050ad78b338aa771fe08
Mike Munroe <mike@mikemunroe.com>
2014-04-10 11:37:12 -0400

    add some book details and start to show more in template

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,21 @@ export default Ember.Route.extend({
       id: 1,
       title: 'Flash Boys',
       author: 'Michael Lewis',
-      review: 'Great coverage of HFT on Wall Street. Michael Lewis always writes hits.'
+      review: 'Great coverage of HFT on Wall Street. Michael Lewis always writes hits.',
+      rating: 5,
+      amazon_id: '0393244660',
+      url: 'http://www.amazon.com/Flash-Boys-Wall-Street-Revolt/dp/0393244660',
+      image: ''
+    },
+    {
+      id: 2,
+      title: 'Getting to Yes: Negotiating Agreement Without Giving In',
+      author: 'William Ury',
+      review: 'On negotiating.',
+      rating: 4,
+      amazon_id: '0143118757',
+      url: 'http://www.amazon.com/Getting-Yes-Negotiating-Agreement-Without/dp/0143118757/ref=sr_1_1?s=books&ie=UTF8&qid=1397143909&sr=1-1&keywords=Getting+to+Yes',
+      image: ''
     }];
   }
 });
