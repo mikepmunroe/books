75a98e7d8174e1fee2a775858b09e3596e546185
Mike Munroe <mike@mikemunroe.com>
2014-04-13 21:37:06 -0400

    add genres to home page

---
+++ app/controllers/index.js
@@ -0,0 +1,5 @@
+var BooksController = Ember.ArrayController.extend({
+  sortProperties: ['title']
+});
+
+export default BooksController;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,12 @@
 export default Ember.Route.extend({
   model: function() {
-    return this.store.findAll('book')
+    return Ember.RSVP.hash({
+      books: this.store.findAll('book'),
+      genres: this.store.findAll('genre'),
+    });
+  },
+  setupController: function(controller, model) {
+    controller.set('books', model.books);
+    controller.set('genres', model.genres);
   }
 });
